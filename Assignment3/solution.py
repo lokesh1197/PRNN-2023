c3ef1535bc864fbc0c38e9e0e01ebda2e72e865e
Lokesh Mohanty <lokesh1197@gmail.com>
2023-04-27 14:36:00 +0530

    solve p2

--- Assignment3/solution.py
+++ Assignment3/solution.py
@@ -256,7 +256,7 @@ class metrics:
 
 # ### Impurity Funcions
 
-# In[49]:
+# In[10]:
 
 
 def gini(data):
@@ -280,7 +280,7 @@ print("Entropy (test): ", entropy(p3["Y"]), entropy(p3['Y'][p3["Y"] == 0]))
 
 # ### CART algorithm
 
-# In[50]:
+# In[16]:
 
 
 # restricting the number of thresholds to increase the speed of the algorithm
@@ -293,10 +293,12 @@ def recommendedThresholds(col, y):
             b.append((a[i][0] + a[i - 1][0]) / 2)
     return b
 
-def recommendedSplit(X, y, impurity):
+def recommendedSplit(X, y, impurity, skip_features_idx=[]):
     best_impurity = 1e9
     best_split = None
     for i, col in enumerate(X.T):
+        if i in skip_features_idx:
+            continue
         ts = recommendedThresholds(col, y)
         for t in ts:
             left, right = y[col <= t], y[col > t]
@@ -311,19 +313,19 @@ recommendedSplit(p3["X"], p3["Y"], gini)
 
 # ### Decision Tree
 
-# In[51]:
+# In[17]:
 
 
 # inner nodes are [i, t, left, right]
 # leaf node is a number (class label)
 # returns a tree in pre-order traversal format
-def buildTree(X, y, impurity, max_depth, depth=0):
+def buildTree(X, y, impurity, max_depth, depth=0, skip_features_idx=[]):
     if depth == max_depth:
         return [np.argmax(np.bincount(y))]
     if np.unique(y).size == 1:
         return [y[0]]
     
-    split = recommendedSplit(X, y, impurity)
+    split = recommendedSplit(X, y, impurity, skip_features_idx)
 
     if split is None:
         return [np.argmax(np.bincount(y))]
@@ -423,7 +425,7 @@ metrics.printCnf(bestResult_p3_entropy[1][2], bestResult_p3_entropy[1][2])
 
 # ### Using Gini as impurity measure
 
-# In[74]:
+# In[43]:
 
 
 depths = [3, 5, 7, 9, 11, 13, 17, 20]
@@ -445,7 +447,7 @@ plt.title("Accuracy vs complexity")
 plt.show()
 
 
-# In[84]:
+# In[44]:
 
 
 print(f"Test Accuracy: {bestResult_p5_gini[1][0] * 100:.2f}%, F1 Score: {np.average(bestResult_p5_gini[1][1]):.2f}")
@@ -489,6 +491,219 @@ metrics.printCnf(bestResult_p5_entropy[1][2], bestResult_p5_entropy[1][2])
 # 
 # Data: `p3, p4, p5`
 
+# ## Implementation
+
+# In[19]:
+
+
+def bootstrap(X, Y):
+    n = len(X)
+    idx = np.random.choice(n, n, replace=False)
+    return X[idx], Y[idx]
+
+def random_forest(X, Y, n_trees=10, depth=5, impurity=gini, n_features=None):
+    trees = []
+    for _ in range(n_trees):
+        X_train, Y_train = bootstrap(X, Y)
+        idx = [] if n_features is None else np.random.choice(X_train.shape[1], X_train.shape[1] - n_features, replace=False)
+        trees.append(buildTree(X_train, Y_train, impurity, depth, skip_features_idx=idx))
+    return trees
+
+def predictForest(X, trees):
+    predictions = np.array([predictAll(X, tree) for tree in trees])
+    return np.array([np.bincount(prediction).argmax() for prediction in predictions.T])
+
+def predictForestProb(X, trees):
+    predictions = np.array([predictAll(X, tree) for tree in trees])
+    return np.array([np.bincount(prediction) / len(prediction) for prediction in predictions.T])
+
+
+# ## Experiment on P3 data
+
+# ### Number of trees: 10, Number of features: 10
+
+# In[27]:
+
+
+trees = random_forest(p3["X"], p3["Y"], n_trees=10, depth=15, impurity=gini)
+metrics.print(predictForest(p3["X"], trees), p3["Y"], predictForest(p3["X_test"], trees), p3["Y_test"], visualize=True)
+
+
+# ### Number of trees: 15, Number of features: 10
+
+# In[28]:
+
+
+trees = random_forest(p3["X"], p3["Y"], n_trees=15, depth=15, impurity=gini)
+metrics.print(predictForest(p3["X"], trees), p3["Y"], predictForest(p3["X_test"], trees), p3["Y_test"], visualize=True)
+
+
+# ### Number of trees: 20, Number of features: 10
+
+# In[29]:
+
+
+trees = random_forest(p3["X"], p3["Y"], n_trees=20, depth=15, impurity=gini)
+metrics.print(predictForest(p3["X"], trees), p3["Y"], predictForest(p3["X_test"], trees), p3["Y_test"], visualize=True)
+
+
+# ### Number of trees: 25, Number of features: 10
+
+# In[30]:
+
+
+trees = random_forest(p3["X"], p3["Y"], n_trees=25, depth=15, impurity=gini)
+metrics.print(predictForest(p3["X"], trees), p3["Y"], predictForest(p3["X_test"], trees), p3["Y_test"], visualize=True)
+
+
+# ### Number of trees: 20, Number of features: 7
+
+# In[31]:
+
+
+trees = random_forest(p3["X"], p3["Y"], n_trees=20, depth=15, impurity=gini, n_features=7)
+metrics.print(predictForest(p3["X"], trees), p3["Y"], predictForest(p3["X_test"], trees), p3["Y_test"], visualize=True)
+
+
+# ### Number of trees: 20, Number of features: 5
+
+# In[32]:
+
+
+trees = random_forest(p3["X"], p3["Y"], n_trees=20, depth=15, impurity=gini, n_features=5)
+metrics.print(predictForest(p3["X"], trees), p3["Y"], predictForest(p3["X_test"], trees), p3["Y_test"], visualize=True)
+
+
+# ### Number of trees: 20, Number of features: 3
+
+# In[33]:
+
+
+trees = random_forest(p3["X"], p3["Y"], n_trees=20, depth=15, impurity=gini, n_features=3)
+metrics.print(predictForest(p3["X"], trees), p3["Y"], predictForest(p3["X_test"], trees), p3["Y_test"], visualize=True)
+
+
+# ## Experiment on P4 data
+
+# ### Number of trees: 10, Number of features: 64
+
+# In[34]:
+
+
+trees = random_forest(p4["X"], p4["Y"], n_trees=10, depth=15, impurity=gini)
+metrics.print(predictForest(p4["X"], trees), p4["Y"], predictForest(p4["X_test"], trees), p4["Y_test"], visualize=True)
+
+
+# ### Number of trees: 15, Number of features: 64
+
+# In[35]:
+
+
+trees = random_forest(p4["X"], p4["Y"], n_trees=15, depth=15, impurity=gini)
+metrics.print(predictForest(p4["X"], trees), p4["Y"], predictForest(p4["X_test"], trees), p4["Y_test"], visualize=True)
+
+
+# ### Number of trees: 20, Number of features: 64
+
+# In[36]:
+
+
+trees = random_forest(p4["X"], p4["Y"], n_trees=20, depth=15, impurity=gini)
+metrics.print(predictForest(p4["X"], trees), p4["Y"], predictForest(p4["X_test"], trees), p4["Y_test"], visualize=True)
+
+
+# ### Number of trees: 25, Number of features: 64
+
+# In[37]:
+
+
+trees = random_forest(p4["X"], p4["Y"], n_trees=25, depth=15, impurity=gini)
+metrics.print(predictForest(p4["X"], trees), p4["Y"], predictForest(p4["X_test"], trees), p4["Y_test"], visualize=True)
+
+
+# ### Number of trees: 20, Number of features: 16
+
+# In[43]:
+
+
+trees = random_forest(p4["X"], p4["Y"], n_trees=20, depth=15, impurity=gini, n_features=16)
+metrics.print(predictForest(p4["X"], trees), p4["Y"], predictForest(p4["X_test"], trees), p4["Y_test"], visualize=True)
+
+
+# ### Number of trees: 20, Number of features: 4
+
+# In[44]:
+
+
+trees = random_forest(p4["X"], p4["Y"], n_trees=20, depth=15, impurity=gini, n_features=4)
+metrics.print(predictForest(p4["X"], trees), p4["Y"], predictForest(p4["X_test"], trees), p4["Y_test"], visualize=True)
+
+
+# ## Experiment on P5 data
+
+# ### Number of trees: 10, Number of features: 10
+
+# In[25]:
+
+
+trees = random_forest(p5["X"], p5["Y"], n_trees=10, depth=15, impurity=gini)
+metrics.print(predictForest(p5["X"], trees), p5["Y"], predictForest(p5["X_test"], trees), p5["Y_test"], visualize=True)
+
+
+# ### Number of trees: 15, Number of features: 10
+
+# In[50]:
+
+
+trees = random_forest(p5["X"], p5["Y"], n_trees=15, depth=15, impurity=gini)
+metrics.print(predictForest(p5["X"], trees), p5["Y"], predictForest(p5["X_test"], trees), p5["Y_test"], visualize=True)
+
+
+# ### Number of trees: 20, Number of features: 10
+
+# In[51]:
+
+
+trees = random_forest(p5["X"], p5["Y"], n_trees=20, depth=15, impurity=gini)
+metrics.print(predictForest(p5["X"], trees), p5["Y"], predictForest(p5["X_test"], trees), p5["Y_test"], visualize=True)
+
+
+# ### Number of trees: 25, Number of features: 10
+
+# In[26]:
+
+
+trees = random_forest(p5["X"], p5["Y"], n_trees=25, depth=15, impurity=gini)
+metrics.print(predictForest(p5["X"], trees), p5["Y"], predictForest(p5["X_test"], trees), p5["Y_test"], visualize=True)
+
+
+# ### Number of trees: 20, Number of features: 7
+
+# In[22]:
+
+
+trees = random_forest(p5["X"], p5["Y"], n_trees=20, depth=15, impurity=gini, n_features=7)
+metrics.print(predictForest(p5["X"], trees), p5["Y"], predictForest(p5["X_test"], trees), p5["Y_test"], visualize=True)
+
+
+# ### Number of trees: 20, Number of features: 5
+
+# In[23]:
+
+
+trees = random_forest(p5["X"], p5["Y"], n_trees=20, depth=15, impurity=gini, n_features=5)
+metrics.print(predictForest(p5["X"], trees), p5["Y"], predictForest(p5["X_test"], trees), p5["Y_test"], visualize=True)
+
+
+# ### Number of trees: 20, Number of features: 3
+
+# In[24]:
+
+
+trees = random_forest(p5["X"], p5["Y"], n_trees=20, depth=15, impurity=gini, n_features=3)
+metrics.print(predictForest(p5["X"], trees), p5["Y"], predictForest(p5["X_test"], trees), p5["Y_test"], visualize=True)
+
+
 # # Problem 3
 # 
 # Implement the Adaboost algorithm with at least 3 learners and one of them must be a Neural Network (MLP/CNN). 
@@ -498,6 +713,33 @@ metrics.printCnf(bestResult_p5_entropy[1][2], bestResult_p5_entropy[1][2])
 # Plot the convergence of train error as a function of the number of learners.
 # 
 # Data: `p3, p4, p5`
+
+# In[39]:
+
+
+# adaboost with numpy
+def adaboost(X, Y, T, depth, stump):
+    m, n = X.shape
+    w = np.ones(m) / m
+    trees = []
+    alphas = []
+    for _ in range(T):
+        tree = stump(X, Y, w, depth)
+        trees.append(tree)
+        pred = predictAll(X, tree)
+        err = np.sum(w * (pred != Y))
+        alpha = 0.5 * np.log((1 - err) / (err + 2))
+        alphas.append(alpha)
+        w = w * np.exp(-alpha * Y * pred)
+        w = w / np.sum(w)
+    return trees, alphas
+
+def predictAdaBoost(X, trees, alphas):
+    return np.sign(np.sum([alpha * predictAll(X, tree) for alpha, tree in zip(alphas, trees)], axis=0))
+
+# trees, alphas = adaboost(p3["X"], p3["Y"], 100, 1, lambda X, Y, w, depth: buildTree(X, Y, gini, depth))
+# metrics.print(predictAdaBoost(p3["X"], trees, alphas), p3["Y"], predictAdaBoost(p3["X_test"], trees, alphas), p3["Y_test"], visualize=True, result=True)
+
 
 # # Problem 4
 # 
@@ -511,7 +753,107 @@ metrics.printCnf(bestResult_p5_entropy[1][2], bestResult_p5_entropy[1][2])
 # 
 # Experiment with different number of cluster sizes and plot the t-sne plots for all cases.
 # 
-# Data: `p3, p4, p5`
+# Data: `p4`
+
+# ## Implementation
+
+# ### GMM based clustering
+
+# In[40]:
+
+
+max_float = np.finfo("float64").max
+max_exp = np.log(max_float)
+
+def normal(x, mean ,cov):
+    n = len(mean)
+    val = -0.5 * (x - mean) @ np.linalg.pinv(cov) @ (x - mean)
+    return np.exp(val) if np.abs(val) < max_exp else (1/max_float) / ((2 * np.pi * np.linalg.det(cov)) ** (n/2) + 1e-8)
+
+# X -> data, k -> number of normal densities
+def gmm(X, k, max_iter = 100, random_seed=42):
+    # m -> number of datapoints, n -> number of features
+    m, n = X.shape
+    # initialization
+    np.random.seed(random_seed)
+    weights = np.random.dirichlet(np.ones(k))
+    means = [np.mean(X) + np.random.rand(n) for _ in range(k)]
+    covs = [np.diag(np.random.rand(n) * 2 + 0.5) for _ in range(k)]
+    
+    for i in range(max_iter):
+        # if np.sum(weights) == 0:
+        #     weights = np.ones(k) / k
+            
+        # Expectation step
+        probs = np.array([[normal(x, means[i], covs[i]) * weights[i] for i in range(k)] for x in X])
+        probs = (probs.T / np.sum(probs, axis=1)).T
+        
+        # Maximization step
+        old_means = means.copy()
+        covs = [(X - means[i]).T @ np.diag(probs.T[i]) @ (X - means[i]) / (np.sum(probs.T[i]) + 1e-8) for i in range(k)]
+        means = [X.T @ probs.T[i] / (np.sum(probs.T[i]) + 1e-8)  for i in range(k)]
+        weights = np.sum(probs, axis=0) / m
+        
+        if np.linalg.norm(np.array(means) - np.array(old_means)) < 1e-8:
+            break
+        
+    return weights, means, covs
+
+
+# ### K-means clustering
+
+# In[41]:
+
+
+# k-means clustering using numpy
+def kmeans(X, k, max_iter=100):
+    # initialize centroids
+    centroids = np.random.uniform(low=X.min(axis=0), high=X.max(axis=0), size=(k, X.shape[1]))
+    for _ in range(max_iter):
+        # calculate distance of each point from each centroid
+        distances = np.linalg.norm(X[:, None] - centroids, axis=-1)
+        # assign each point to the closest centroid
+        clusters = np.argmin(distances, axis=-1)
+        # update centroids
+        centroids = np.array([np.mean(X[clusters == i], axis=0) for i in range(k)])
+    return clusters, centroids
+
+
+# In[42]:
+
+
+# mutual information score between two variables
+def mutualInfo(x, y):
+    # joint distribution
+    joint = np.histogram2d(x, y, bins=10)[0]
+    # marginal distribution
+    marginal_x = np.histogram(x, bins=10)[0]
+    marginal_y = np.histogram(y, bins=10)[0]
+    # calculate mutual information
+    mi = 0
+    for i in range(joint.shape[0]):
+        for j in range(joint.shape[1]):
+            if joint[i, j] != 0:
+                mi += joint[i, j] * np.log(joint[i, j] / (marginal_x[i] * marginal_y[j]))
+    return mi
+
+def normalized_mutual_info_score(y_true, y_pred):
+    return mutualInfo(y_true, y_pred) / np.sqrt(mutualInfo(y_true, y_true) * mutualInfo(y_pred, y_pred))
+
+
+# In[ ]:
+
+
+
+
+
+# ## Experiment
+
+# In[ ]:
+
+
+
+
 
 # # Problem 5
 # 
@@ -520,6 +862,8 @@ metrics.printCnf(bestResult_p5_entropy[1][2], bestResult_p5_entropy[1][2])
 # Plot the data variance as a function of the number of principal components.
 # 
 # Data: `p3, p4, p5`
+
+# ## Implementation
 
 # In[10]:
 
@@ -539,23 +883,10 @@ def pca(X):
 def pcaTransform(X, eigVecs, k):
     return np.dot(X, eigVecs[:, :k])
 
-# data variance as a function of number of principal components
-def varianceCumSum(X):
-    eigVecs, eigVals = pca(X)
-    eigVals = eigVals[:k]
-    return np.sum(eigVals) / np.sum(eigVals)
-
 
 # ## Data variance vs number of principal components
 
-# In[17]:
-
-
-a = [4, 3, 2, 1]
-np.cumsum(a)
-
-
-# In[20]:
+# In[21]:
 
 
 fig, ax = plt.subplots(1, 3, figsize=(18, 5))
@@ -563,7 +894,7 @@ _, eigVals = pca(p3["X"])
 eigValCumsum = np.cumsum(eigVals)
 eigValCumsum = eigValCumsum / eigValCumsum[-1]
 ax[0].plot(np.arange(1, len(eigVals) + 1), eigValCumsum, marker='o')
-ax[0].set_xlabel("k -->")
+ax[0].set_xlabel("Number of components -->")
 ax[0].set_ylabel("Fraction of variance retained -->")
 ax[0].set_title("P3 data")
 
@@ -571,7 +902,7 @@ _, eigVals = pca(p4["X"])
 eigValCumsum = np.cumsum(eigVals)
 eigValCumsum = eigValCumsum / eigValCumsum[-1]
 ax[1].plot(np.arange(1, len(eigVals) + 1), eigValCumsum, marker='o')
-ax[1].set_xlabel("k -->")
+ax[1].set_xlabel("Number of components -->")
 ax[1].set_ylabel("Fraction of variance retained -->")
 ax[1].set_title("P4 data")
 
@@ -579,7 +910,7 @@ _, eigVals = pca(p5["X"])
 eigValCumsum = np.cumsum(eigVals)
 eigValCumsum = eigValCumsum / eigValCumsum[-1]
 ax[2].plot(np.arange(1, len(eigVals) + 1), eigValCumsum, marker='o')
-ax[2].set_xlabel("k -->")
+ax[2].set_xlabel("Number of components -->")
 ax[2].set_ylabel("Fraction of variance retained -->")
 ax[2].set_title("P5 data")
 
